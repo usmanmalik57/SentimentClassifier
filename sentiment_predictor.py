ddca1d03d623971691e9322e5e4e6ffedab87709
mani <usmanmalik57@gmail.com>
2024-02-12 15:49:21 +0100

    titled updated from main

--- sentiment_predictor.py
+++ sentiment_predictor.py
@@ -6,7 +6,7 @@ rf_classifier, tfidf_vectorizer = dataloader.get_classifier_tfidf()
 
 # Define the Streamlit app
 def main():
-    st.title("Movie Sentiment Analysis")
+    st.title("Movie Sentiment Analysis Update from main!")
 
     # Create a text input widget for user input
     user_input = st.text_area("Enter a review:")
