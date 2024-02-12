56822029ad42bce8bd70f3bb5f9f513fb1c503fd
mani <usmanmalik57@gmail.com>
2024-02-12 16:12:40 +0100

    added feature 2

--- sentiment_predictor.py
+++ sentiment_predictor.py
@@ -12,6 +12,7 @@ def main():
     user_input = st.text_area("Enter a review:")
 
     st.text("Message from feature 1")
+    st.text("Message from feature 2")
 
     if st.button("Get Sentiment"):
         if user_input:
